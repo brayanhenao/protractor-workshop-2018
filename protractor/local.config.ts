8f0291b2fe85b1d322d526ac13515e1cd6f7e05c
brayanhenao <bryanhenao96@gmail.com>
2019-01-16 09:19:24 -0500

    added points 3 and 4 from the guide

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -1,10 +1,12 @@
-import { Config, browser } from "protractor";
+import { Config, browser, } from "protractor";
+import { reporter } from './helper/reporter';
 
 export const config: Config = {
   framework: "jasmine",
   specs: ["../test/google.spec.js"],
-  seleniumAddress: "http://localhost:4444/wd/hub",
+  SELENIUM_PROMISE_MANAGER : false,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
+    reporter();
   }
 };
